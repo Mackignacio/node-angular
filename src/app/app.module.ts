91ae7eb8a4c9c6731a59087ef00436b6df6e69e2
Mack_Ignacio <mack_ignacio@yahoo.com>
2018-09-29 06:13:42 +0800

    Add material design

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,21 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+// Forms
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { MatFormFieldModule } from "@angular/material/form-field";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { UserListComponent } from './components/user-list/user-list.component';
-import { UserAddComponent } from './components/user-add/user-add.component';
-import { UserEditComponent } from './components/user-edit/user-edit.component';
-
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { UserListComponent } from "./components/user-list/user-list.component";
+import { UserAddComponent } from "./components/user-add/user-add.component";
+import { UserEditComponent } from "./components/user-edit/user-edit.component";
+import { DatabaseService } from "./services/database.service";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { MaterialModule } from "./material.module";
 @NgModule({
-  declarations: [
-    AppComponent,
-    UserListComponent,
-    UserAddComponent,
-    UserEditComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  declarations: [AppComponent, UserListComponent, UserAddComponent, UserEditComponent],
+  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule, MaterialModule, FormsModule, ReactiveFormsModule, MatFormFieldModule],
+  providers: [DatabaseService],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
